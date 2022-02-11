1d03ccd875b2b97585922f7c14ff57c235621351
Violet Stewart <vkazoo2@gmail.com>
2022-02-10 16:02:27 -0800

    hooked up entry and use entries

--- src/components/EntryList/EntryList.jsx
+++ src/components/EntryList/EntryList.jsx
@@ -1,5 +1,20 @@
-import React from 'react';
+import { useEntries } from '../../Context/EntryContext';
+import Entry from '../Entry/Entry';
 
 export default function EntryList() {
-  return <div></div>;
+  const { entries } = useEntries();
+
+  return (
+    <div>
+      <ul>
+        {entries.map((entry) => {
+          return (
+            <li key={`${entry.name}-${entry.message}`}>
+              <Entry entry={entry} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { EntryProvider } from '../../Context/EntryContext';
 import GuestBook from '../../components/GuestBook/GuestBook';
+import EntryList from '../../components/EntryList/EntryList';
 
 export default function Home() {
   return (
     <div>
       <EntryProvider>
         <GuestBook />
-        {/* <EntryList /> */}
+        <EntryList />
         <p>this is where guestbook and entry list go</p>
       </EntryProvider>
     </div>
