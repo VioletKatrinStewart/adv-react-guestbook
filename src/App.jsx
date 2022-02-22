b855e4ce516a143562d37ef8f572f584a1191c41
Violet Stewart <vkazoo2@gmail.com>
2022-02-22 10:13:50 -0800

    made a plan and set up app jsx routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Auth from './views/Auth/Auth';
+
 import Layout from './views/Layout/Layout';
 import Home from './views/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Layout>
-        <Home />
-      </Layout>
+      <BrowserRouter>
+        <Layout>
+          <Switch>
+            <Route path="/login">
+              <Auth />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Layout>
+      </BrowserRouter>
     </div>
   );
 }
